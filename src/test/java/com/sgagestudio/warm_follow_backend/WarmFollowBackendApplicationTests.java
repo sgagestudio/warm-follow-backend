3c4df277bbe11bd6e22a389813ec107d1728d255
sgagestudio <sgagestudio@gmail.com>
2026-01-19 10:35:24 +0100

    añadir esqueleto

---
+++ src/test/java/com/sgagestudio/warm_follow_backend/WarmFollowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgagestudio.warm_follow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarmFollowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
